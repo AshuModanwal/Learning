f8b7c39769b4b728c0827dacdc2c4d4c52412720
AshuModanwal <ashu.modanwal15@gmail.com>
2026-04-04 10:03:22 +0530

    solved buy & sell stock first question

--- Java/dsa/sde_sheet/BestTimeToBuySellStock.java
+++ Java/dsa/sde_sheet/BestTimeToBuySellStock.java
@@ -1,4 +1,28 @@
 package dsa.sde_sheet;
 
 public class BestTimeToBuySellStock {
+
+    private int buySellStockFirst(int []prices){
+        int n = prices.length;
+        int profit = 0;
+        int buyPrice = Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++){
+            if(prices[i]<buyPrice && i!=n-1){
+                buyPrice = prices[i];
+            }
+
+            if(buyPrice!=Integer.MAX_VALUE){
+                profit = Math.max(profit, prices[i] - buyPrice);
+            }
+        }
+
+        return profit;
+    }
+    public static void main(String []args){
+        int [] prices = {7,1,5,3,6,4};
+        BestTimeToBuySellStock ob = new BestTimeToBuySellStock();
+        int profit = ob.buySellStockFirst(prices);
+        System.out.println("buySellStockFirst profit: "+profit);
+    }
 }
