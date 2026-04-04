c733faf26008c98e51cc8487d2663c164191255d
AshuModanwal <ashu.modanwal15@gmail.com>
2026-04-04 10:12:44 +0530

    solved buy & sell stock second  question

--- Java/dsa/sde_sheet/BestTimeToBuySellStock.java
+++ Java/dsa/sde_sheet/BestTimeToBuySellStock.java
@@ -19,10 +19,33 @@ public class BestTimeToBuySellStock {
 
         return profit;
     }
+
+    private int buySellStockSecond(int []prices){
+        int n = prices.length;
+        int profit = 0;
+        int buyPrice =0;
+        boolean baught = false;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!baught) {
+                buyPrice = prices[i];
+                baught = true;
+            }
+
+            if (baught && (i == n - 1 || prices[i] > prices[i + 1])) {
+                if (prices[i] > buyPrice) {
+                    profit += prices[i] - buyPrice;
+                }
+                baught = false;
+            }
+        }
+        return  profit;
+    }
     public static void main(String []args){
         int [] prices = {7,1,5,3,6,4};
         BestTimeToBuySellStock ob = new BestTimeToBuySellStock();
-        int profit = ob.buySellStockFirst(prices);
-        System.out.println("buySellStockFirst profit: "+profit);
+        System.out.println("buySellStockFirst profit: "+ob.buySellStockFirst(prices));
+        System.out.println("buySellStockSecond profit: "+ob.buySellStockSecond(prices));
     }
 }
