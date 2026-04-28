fff5f21ccbbcc5382a7ff07dab2d5e1206786375
AshuModanwal <ashu.modanwal15@gmail.com>
2026-04-28 20:39:47 +0530

    Removed build files and updated gitignore

--- Java/dsa/sde_sheet/dp/Fibonacci.java
+++ Java/dsa/sde_sheet/dp/Fibonacci.java
@@ -2,24 +2,57 @@ package dsa.sde_sheet.dp;
 
 public class Fibonacci {
 
-    static int fib(int n, int [] dp){
+    // Time Complexiy: O(n)
+    // Space Complexity: O(n)
+    static int fibMem(int n, int [] dp){
         if(n <= 1)
             return  n;
         if(dp[n]!=-1)
             return dp[n];
 
-        dp[n] = fib(n-1, dp) + fib(n-2, dp);
+        dp[n] = fibMem(n-1, dp) + fibMem(n-2, dp);
         return dp[n];
+    }
+
+    // Time Complexiy: O(n)
+    // Space Complexity: O(n)
+    static int fibTab(int n){
+        int []dp = new int[n+1];
+
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+
+    // Time Complexiy: O(n)
+    // Space Complexity: O(1)
+    static  int fibOpt(int n){
+        int prev1 =1, prev2=0;
+
+        for(int i=2; i<=n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1=curr;
+        }
+
+        return prev1;
     }
 
     static void main() {
 
-        int n = 8,i=0;
+        int n = 12,i=0;
         int [] dp = new int[n+1];
         while(i<=n)
             dp[i++]=-1;
 
-        System.out.println(n+"th number in fib series: "+fib(n, dp));
+        System.out.println(n+"th number in fib series by Memoization: "+fibMem(n, dp));
+        System.out.println(n+"th number in fib series by Tabuaization: "+fibTab(n));
+        System.out.println(n+"th number in fib series by Optimization: "+fibOpt(n));
 
 
     }
