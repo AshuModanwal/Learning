8c71db818ce2a78fd59ed6c1b22d80b3afa34a6d
AshuModanwal <ashu.modanwal15@gmail.com>
2026-04-28 20:24:12 +0530

    Started the DP Series

--- Java/dsa/sde_sheet/SecondLargest.java
+++ Java/dsa/sde_sheet/SecondLargest.java
@@ -1,4 +1,28 @@
 package dsa.sde_sheet;
 
 public class SecondLargest {
+
+    static int secondLargest(int [] arr){
+        int lar = arr[0];
+        int sec = Integer.MIN_VALUE;
+
+        for(int i: arr){
+            if(i > lar){
+                sec = lar;
+                lar = i;
+            }
+
+            if(i > sec && i != lar){
+                sec = i;
+            }
+        }
+        return  sec;
+    }
+
+    static void main() {
+        int [] arr = {3,2,1,5,4,6};
+
+        int sec = secondLargest(arr);
+        System.out.println("Second Largest: "+ sec);
+    }
 }
--- Java/dsa/sde_sheet/dp/Fibonacci.java
+++ Java/dsa/sde_sheet/dp/Fibonacci.java
@@ -1,4 +1,26 @@
 package dsa.sde_sheet.dp;
 
 public class Fibonacci {
+
+    static int fib(int n, int [] dp){
+        if(n <= 1)
+            return  n;
+        if(dp[n]!=-1)
+            return dp[n];
+
+        dp[n] = fib(n-1, dp) + fib(n-2, dp);
+        return dp[n];
+    }
+
+    static void main() {
+
+        int n = 8,i=0;
+        int [] dp = new int[n+1];
+        while(i<=n)
+            dp[i++]=-1;
+
+        System.out.println(n+"th number in fib series: "+fib(n, dp));
+
+
+    }
 }
